936fae7ae7031d0c9fd8dc747b56ed4cb03e83fa
Dasha <dasham.cris@gmail.com>
2024-09-10 17:20:28 +0300

    changed the desing

--- script.js
+++ script.js
@@ -4,81 +4,81 @@ let minutes = 0;
 let seconds = 0;
 let isRunning = false;
 
-const display = document.querySelector('#display');
-const stopStart = document.querySelector('#startstop');
-const reset = document.querySelector('#reset');
-const lapBtn = document.querySelector('#lap');
-const lapContainer = document.querySelector('#lap-times');
+const display = document.querySelector("#display");
+const stopStart = document.querySelector("#startstop");
+const reset = document.querySelector("#reset");
+const lapBtn = document.querySelector("#lap");
+const lapContainer = document.querySelector("#lap-times");
 
-function updateDisplay(){
-    const formatTime = 
-    (hours < 10 ? "0" + hours : hours) + ":" +
-    (minutes < 10 ? "0" + minutes : minutes) + ":" +
+function updateDisplay() {
+  const formatTime =
+    (hours < 10 ? "0" + hours : hours) +
+    ":" +
+    (minutes < 10 ? "0" + minutes : minutes) +
+    ":" +
     (seconds < 10 ? "0" + seconds : seconds);
-     
-    display.textContent = formatTime;
 
-    document.title = formatTime;
-    
+  display.textContent = formatTime;
 
+  document.title = formatTime;
 }
-lapContainer.classList.remove('shadoow');
+lapContainer.classList.remove("shadoow");
 
-function startTimer(){
-        timer = setInterval(() => {
-            seconds ++;
-            if(seconds === 60){
-                seconds = 0;
-                minutes ++;
-            }
-            if(minutes === 60){
-                minutes = 0;
-                hours ++;
-            } updateDisplay();
-        }, 1000,)
+function startTimer() {
+  timer = setInterval(() => {
+    seconds++;
+    if (seconds === 60) {
+      seconds = 0;
+      minutes++;
     }
-
-function stopTimer (){
-    clearInterval(timer);
-
-};
-
-function startStop(){
-    if(isRunning){
-        stopTimer();
-        stopStart.textContent = 'Start';
-    } else{
-        startTimer();
-        stopStart.textContent = 'Stop';
+    if (minutes === 60) {
+      minutes = 0;
+      hours++;
     }
-    isRunning = !isRunning;
-}
-function resetTimer(){
-    isRunning = false;
-    clearInterval(timer);
-    stopStart.textContent = 'Start';
-    hours = 0;
-    minutes = 0;
-    seconds = 0;
     updateDisplay();
-    lapContainer.textContent = '';
-lapContainer.classList.remove('shadoow');
-document.title = 'Stopwatch'
-
+  }, 1000);
 }
 
-function addLap(){
-    if(isRunning){
-          const lapTime = display.textContent;
-    const lapElem = document.createElement('div');
-    lapElem.textContent = `Lap: ${lapTime}`;
-    lapContainer.appendChild(lapElem);
-    lapContainer.classList.add('shadoow');
+function stopTimer() {
+  clearInterval(timer);
 }
-    }
-  
 
+function startStop() {
+  if (isRunning) {
+    stopTimer();
+    stopStart.innerHTML = '<img src="/img/mdi_play.svg" alt="Start">';
+  } else {
+    startTimer();
+    stopStart.innerHTML =
+      '<img src="/img/material-symbols_pause.svg" alt="Stop">';
+  }
+  isRunning = !isRunning;
+}
+function resetTimer() {
+  isRunning = false;
+  clearInterval(timer);
+  stopStart.innerHTML = '<img src="/img/mdi_play.svg" alt="Start">';
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+  updateDisplay();
+  lapContainer.textContent = "";
+  document.title = "Stopwatch";
+}
 
-stopStart.addEventListener('click', startStop);
-reset.addEventListener('click', resetTimer);
-lapBtn.addEventListener('click', addLap);
+function addLap() {
+    if (isRunning) {
+       
+        if (lapContainer.children.length >= 3) {
+          return;
+        }
+        const lapTime = display.textContent;
+        const lapElem = document.createElement("div");
+        lapElem.textContent = `Lap: ${lapTime}`;
+        lapContainer.appendChild(lapElem);
+      }
+}
+
+stopStart.addEventListener("click", startStop);
+reset.addEventListener("click", resetTimer);
+lapBtn.addEventListener("click", addLap);
